83eaacb44e3085d9ee91b5206db8457f114a5e8e
mntt <64373240+mntt@users.noreply.github.com>
2021-04-26 21:36:24 +0300

    Update Colours.cs

--- Colours.cs
+++ Colours.cs
@@ -1,14 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace cryptoFinance
 {
     public static class Colours
     {
+        public static Color labelColor = Color.FromArgb(255, 255, 255);
+        public static Color panelBackground = Color.FromArgb(29, 33, 41);
+        public static Color formBackground = Color.FromArgb(59, 68, 85);
+        public static Color cellBack = Color.FromArgb(80, 91, 114);
+        public static Color grid = Color.FromArgb(116, 201, 204);
+        public static Color alternateCellBack = Color.FromArgb(99, 114, 141);
+        public static Color dataGridColumnHeaderBack = Color.FromArgb(29, 33, 41);
+        public static Color buttonBorder = Color.FromArgb(59, 68, 85);
+        public static Color buttonMouseOverBack = Color.FromArgb(54, 62, 78);
+        public static System.Windows.Media.Color chartGrid = System.Windows.Media.Color.FromRgb(255, 255, 255);
+        public static System.Windows.Media.Color chartLabels = System.Windows.Media.Color.FromRgb(255, 255, 255);
+        public static System.Windows.Media.Color tooltipLabels = System.Windows.Media.Color.FromRgb(59, 68, 85);
+
         public static Color Transparent()
         {
             return Color.Transparent;
@@ -21,3 +29,4 @@ namespace cryptoFinance
 
     }
 }
+
